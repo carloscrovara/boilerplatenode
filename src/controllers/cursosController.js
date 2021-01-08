a23e15fc922f39d9a8a91df1bf5c4a8ac435490f
carloscrovara <carloscrovara@gmail.com>
2021-01-08 13:40:24 -0300

    Version 1.0

---
+++ src/controllers/cursosController.js
@@ -0,0 +1,16 @@
+const cursosCtrl = {};
+
+cursosCtrl.index = (req, res) => {
+res.json("hola llegué al controller");
+}
+
+cursosCtrl.test = (req, res) =>{
+    res.json("hola soy otra ruta");
+}
+
+cursosCtrl.formulario = (req, res) =>{
+    let hola = req.body;
+    res.json(hola)
+}
+
+module.exports = cursosCtrl;
